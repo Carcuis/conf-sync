416ecae34105ed8f47293204f51f31dbc7efd727
Carcuis <ctx1256@foxmail.com>
2020-12-01 21:26:16 +0800

    On Ubuntu add vundle-setup

--- setup_dependency.zsh
+++ setup_dependency.zsh
@@ -4,3 +4,4 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 #zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
