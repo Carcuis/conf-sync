ce919a97d8a2be312652bc20f43da18bd4aa9bc3
Carcuis <ctx1256@foxmail.com>
2020-12-31 23:20:38 +0800

    On WSL add oh-my-zsh setup commond

--- setup_dependency.zsh
+++ setup_dependency.zsh
@@ -1,4 +1,6 @@
 #!/usr/bin/zsh
+#oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 #zsh-syntax-highlighting
