f7f7813e1bd4247e118727b20d0b69a847cfb5d5
Carcuis <ctx1256@foxmail.com>
2020-12-31 23:50:03 +0800

    On WSL add vifm-colors setup command

--- setup_dependency.zsh
+++ setup_dependency.zsh
@@ -9,3 +9,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 #vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#vifm-colors
+rm -rf ~/.config/vifm/colors
+git clone https://github.com/vifm/vifm-colors ~/.config/vifm/colors
