464f41de94666c9e4d38f9658187bda2c6e4f675
Carcuis <ctx1256@foxmail.com>
2021-02-16 23:36:31 +0800

    On Darwin change vundle.vim to vim-plug

--- setup_dependency.zsh
+++ setup_dependency.zsh
@@ -11,8 +11,8 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 #powerlevel10k
 git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-#vundle.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 #vifm-colors
 if [[ ! -e "$HOME/.config/vifm/colors/ph.vifm" ]]; then
     rm -rf ~/.config/vifm/colors
