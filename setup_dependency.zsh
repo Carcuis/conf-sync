f15d8349d4cf74cd5963bed06fa19c0596f73cfa
Carcuis <ctx1256@foxmail.com>
2021-02-22 17:38:56 +0800

    On Darwin modify vifm-setup

--- setup_dependency.zsh
+++ setup_dependency.zsh
@@ -15,8 +15,11 @@ git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$H
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 #vifm-colors
 if [[ ! -e "$HOME/.config/vifm/colors/ph.vifm" ]]; then
-    rm -rf ~/.config/vifm/colors
+    vifm
+    mv ~/.config/vifm/colors/Default.vifm ~/.config/Default.vifm.bak
+    rmdir ~/.config/vifm/colors
     git clone https://github.com/vifm/vifm-colors ~/.config/vifm/colors
+    mv ~/.config/Default.vifm.bak ~/.config/vifm/colors/
 else
     echo "Vifm colorshemes already exist."
 fi
