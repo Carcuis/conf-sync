8f6a7faa411999d12877528873204379730a9ba2
Carcuis <ctx1256@foxmail.com>
2021-06-24 20:43:59 +0800

    Add neovim support

--- check_consistency.sh
+++ check_consistency.sh
@@ -10,6 +10,28 @@ zshrc_sync_status=$?
 diff $vimrc_remote $vimrc_local > /dev/null
 vimrc_sync_status=$?
 
+has_nvim=0
+has_vim=0
+if [[ $(command -v nvim > /dev/null) -eq 0 ]]; then
+    has_nvim=1
+    diff_tool="nvim -d"
+elif [[ $(command -v vim > /dev/null) -eq 0 ]]; then
+    has_vim=1
+    diff_tool="vimdiff"
+else
+    echo -e "\033[31mNo vim or neovim found on your device.\nAborting...\033[0m"
+    exit 1
+fi
+
+function DiffFunc
+{
+    if [ $has_nvim == 1 ]; then
+        nvim -d $1 $2
+    elif [ $has_vim == 1 ]; then
+        vimdiff $1 $2
+    fi
+}
+
 check_status=0
 
 if [ $zshrc_sync_status == 0 ] && [ $vimrc_sync_status == 0 ]; then
@@ -17,16 +39,16 @@ if [ $zshrc_sync_status == 0 ] && [ $vimrc_sync_status == 0 ]; then
     check_status=1
 else
     if [ $zshrc_sync_status != 0 ]; then
-        read -s -n1 -p "Zshrc unsynchronized. Edit in vimdiff? [Y/n] " user_input
+        read -s -n1 -p "Zshrc unsynchronized. Edit with $diff_tool ? [Y/n] " user_input
         if [ "$user_input" == "y" ] || [ "$user_input" == "" ]; then
             echo ""
-            vimdiff $zshrc_remote $zshrc_local
+            DiffFunc $zshrc_remote $zshrc_local
             diff $zshrc_remote $zshrc_local > /dev/null
             zshrc_sync_status=$?
             if [ $zshrc_sync_status == 0 ]; then
                 echo -e "\nZshrc is now synced."
             else
-                echo -e "\nZshrc is still unsynchronized.\n--Use \" vimdiff $zshrc_remote $zshrc_local \" later\n--or try to rerun this wizard"
+                echo -e "\nZshrc is still unsynchronized.\n--Use \" $diff_tool $zshrc_remote $zshrc_local \" later\n--or try to rerun this wizard"
             fi
         else
             echo -e "\nZshrc is still unsynchronized. Aborting."
@@ -36,16 +58,16 @@ else
     fi
 
     if [ $vimrc_sync_status != 0 ]; then
-        read -s -n1 -p "Vimrc unsynchronized. Edit in vimdiff? [Y/n] " user_input
+        read -s -n1 -p "Vimrc unsynchronized. Edit with $diff_tool ? [Y/n] " user_input
         if [ "$user_input" == "y" ] || [ "$user_input" == "" ]; then
             echo ""
-            vimdiff $vimrc_remote $vimrc_local
+            DiffFunc $vimrc_remote $vimrc_local
             diff $vimrc_remote $vimrc_local > /dev/null
             vimrc_sync_status=$?
             if [ $vimrc_sync_status == 0 ]; then
                 echo -e "\nVimrc is now synced."
             else
-                echo -e "\nVimrc still unsynchronized.\n--Use \" vimdiff $vimrc_remote $vimrc_local \" later\n--or try to rerun this wizard"
+                echo -e "\nVimrc still unsynchronized.\n--Use \" $diff_tool $vimrc_remote $vimrc_local \" later\n--or try to rerun this wizard"
             fi
         else
             echo -e "\nVimrc still unsynchronized. Aborting."
