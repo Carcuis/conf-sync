e636b3b2b0c3af08134e818cc87f604c829ea4e6
Carcuis <ctx1256@foxmail.com>
2021-09-09 01:12:07 +0800

    On Windows update powershell profile

--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -9,15 +9,39 @@ Set-PSReadlineKeyHandler -Key "Ctrl+d" -Function DeleteCharOrExit
 
 Get-quote|lolcat
 
+If ((Get-Command nvim).length -gt 0)
+{
+    $hasNvim = $true
+} ElseIf ((Get-Command vim).length -gt 0)
+{
+    $hasVim = $true
+} Else
+{
+    Write-Error "No vim or neovim found on your device.`nAborting..."
+    exit
+}
+
+function EditFile
+{
+    param($file)
+    if ($hasNvim)
+    {
+        nvim $file
+    } ElseIf ($hasVim)
+    {
+        vim $file
+    }
+}
+
+function ListAll { Get-ChildItemColorFormatWide -Force }
+function LLAll { Get-ChildItemColor -Force }
 function GoUpOne { Set-Location .. }
 function GoUpTwo { Set-Location ../.. }
 function GoUpThree { Set-Location ../../.. }
 function GoUpFour { Set-Location ../../../.. }
 function OpenCwd { explorer.exe . }
-function EditProfileWithVim { vim $profile }
-function EditProfileWithGVim { gvim $profile }
-function EditVimrcWithVim { vim ~/_vimrc }
-function EditVimrcWithGvim { gvim ~/_vimrc }
+function EditProfile { EditFile $profile }
+function EditVimrc { EditFile $HOME\_vimrc }
 function OpenVifmInPwd { vifm_copy.exe . }
 function ReloadProfile { . $profile }
 function GetAdminPriv { Start-Process pwsh -Verb runAs }
@@ -31,20 +55,18 @@ Set-Alias .. GoUpOne
 Set-Alias ... GoUpTwo
 Set-Alias .... GoUpThree
 Set-Alias ..... GoUpFour
-Set-Alias al Get-ChildItemColorFormatWide
-Set-Alias la Get-ChildItemColorFormatWide
+Set-Alias al ListAll
+Set-Alias la ListAll
 Set-Alias ls Get-ChildItemColorFormatWide
-Set-Alias ll Get-ChildItemColor
+Set-Alias ll LLAll
 Set-Alias which Get-Command
 Set-Alias o explorer.exe
 Set-Alias o. OpenCwd
 Set-Alias touch New-Item
 Set-Alias vi vim
 Set-Alias gvi gvim
-Set-Alias pwshc EditProfileWithVim
-Set-Alias pwshcg EditProfileWithGVim
-Set-Alias vimc EditVimrcWithVim
-Set-Alias gvimc EditVimrcWithGvim
+Set-Alias pwshc EditProfile
+Set-Alias vimc EditVimrc
 Set-Alias nvi nvim
 Set-Alias gnvi nvim-qt
 Set-Alias vifm OpenVifmInPwd
