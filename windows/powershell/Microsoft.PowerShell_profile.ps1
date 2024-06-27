9bb5fd6d847dd773d19ed47c763a7a2286ed9a35
Carcuis <ctx1256@foxmail.com>
2024-06-27 22:05:59 +0800

    Remove chocolatey module in PSProfile

--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -179,12 +179,3 @@ Set-Alias mvx Move-And-Create-Link
 Set-Alias md5sum Sum-MD5
 Set-Alias sha256sum Sum-SHA256
 
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
