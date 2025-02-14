4f69217a08ef40ad693f01e2e444b1a2d9480425
Carcuis <ctx1256@foxmail.com>
2025-02-14 13:26:36 +0800

    fix: update yazi plugin `init.lua` -> `main.lua` (sxyazi/yazi#2168)

--- setup.sh
+++ setup.sh
@@ -237,11 +237,11 @@ function install_yazi_package() {
     fi
 
     # yazi plugins
-    if not_installed_file "$HOME/.config/yazi/plugins/smart-enter.yazi/init.lua" "Yazi plugin smart-enter"; then
+    if not_installed_file "$HOME/.config/yazi/plugins/smart-enter.yazi/main.lua" "Yazi plugin smart-enter"; then
         ya pack -a yazi-rs/plugins:smart-enter
         successfully_installed $? "Yazi plugin smart-enter"
     fi
-    if not_installed_file "$HOME/.config/yazi/plugins/mediainfo.yazi/init.lua" "Yazi plugin mediainfo"; then
+    if not_installed_file "$HOME/.config/yazi/plugins/mediainfo.yazi/main.lua" "Yazi plugin mediainfo"; then
         ya pack -a boydaihungst/mediainfo
         successfully_installed $? "Yazi plugin mediainfo"
     fi
