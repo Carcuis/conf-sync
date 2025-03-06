010ce0c32f9a0d7930df81c48e4663d37fc2cce5
Carcuis <ctx1256@foxmail.com>
2025-03-06 13:05:23 +0800

    On Windows disable lazy.nvim bootstrap in vscode
    chore: format

--- vscode/vscode.nvim.lua
+++ vscode/vscode.nvim.lua
@@ -50,19 +50,26 @@ function M.setup_lazy_nvim()
     local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
     ---@diagnostic disable-next-line: undefined-field
     if not (vim.uv or vim.loop).fs_stat(lazypath) then
-      local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-      local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-      if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-          { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-          { out, "WarningMsg" },
-          { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-      end
+        if vim.g.vscode then
+            vim.notify("vscode nvim: Lazy.nvim not installed, please run `vscnvim` in terminal", vim.log.levels.WARN)
+            return
+        end
+
+        local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+        local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+        if vim.v.shell_error ~= 0 then
+            vim.api.nvim_echo({
+                { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+                { out, "WarningMsg" },
+                { "\nPress any key to exit..." },
+            }, true, {})
+            vim.fn.getchar()
+            os.exit(1)
+        end
     end
     vim.opt.rtp:prepend(lazypath)
+
+    M.load_plugins()
 end
 
 function M.load_plugins()
@@ -329,7 +336,6 @@ function M.setup()
     end
 
     M.setup_lazy_nvim()
-    M.load_plugins()
 end
 
 M.setup()
